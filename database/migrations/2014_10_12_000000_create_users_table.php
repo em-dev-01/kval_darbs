1233a1f9035cfacd55c2598c689db219bd187565
me4454 <me44465@gmail.com>
2023-10-19 20:22:36 +0300

    added auth scaffold

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name');
+            $table->string('company_name');
             $table->string('email')->unique();
+            $table->bigInteger('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role');
+            $table->foreign('role')->references('id')->on('roles');
             $table->rememberToken();
             $table->timestamps();
         });
