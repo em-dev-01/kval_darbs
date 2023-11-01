17e22a6989eab8cbe1cedbd9219dcdc2be267ff0
me4454 <me44465@gmail.com>
2023-11-01 12:19:01 +0200

    fixed Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Enums\Status;
+use App\Enums\StatusEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
